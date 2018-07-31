341b37af25919262ab1bbe2a0a01b58fd07009b5
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-31 09:09:36 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,6 @@
 # require modules here
 
+
 def load_library(file_path)
   library = {"get_meaning" => {}, "get_emoticon" => {} }
   YAML.load_file(file_path).each do |meaning, array|
@@ -13,6 +14,16 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   result = library["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
+
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  result = library["get_meaning"][emoticon]
   if result
     result
   else
